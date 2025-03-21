d876a46860191497d3f3efecba680fca96694557
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-03-21 10:22:52 -0300

    nav corregido

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,27 +1,21 @@
-import { useState } from "react"
-import Dots from "./icons/Dots"
-import Popup from "./Popup"
+import { useState } from "react";
+import Dots from "./icons/Dots";
+import Popup from "./Popup";
 
 const Options = ({ idTask, tasks, setTasks }) => {
-  const [open, setOpen] = useState(false)
-  
-  const handleOptions = (e) => {
-    console.log('option');
-    setOpen(current => !current)
-  }
-  
-  return (
-    <span
-      className="relative"
-      onClick={handleOptions}
-    >
-      <Dots />
-      { open && <Popup 
-          idTask={idTask} 
-          tasks={tasks} 
-          setTasks={setTasks}/> }
-    </span>
-  )
-}
+  const [open, setOpen] = useState(false);
 
-export default Options
+  const handleOptions = (e) => {
+    console.log("option");
+    setOpen((current) => !current);
+  };
+
+  return (
+    <span className="relative" onClick={handleOptions}>
+      <Dots />
+      {open && <Popup idTask={idTask} tasks={tasks} setTasks={setTasks} />}
+    </span>
+  );
+};
+
+export default Options;
